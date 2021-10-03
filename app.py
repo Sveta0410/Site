61cbc5f085bf6ab361fac0fde46f49a768d5a36c
Sveta0410 <90606869+Sveta0410@users.noreply.github.com>
2021-10-03 21:11:09 +0300

    choose language

--- app.py
+++ app.py
@@ -1,31 +1,58 @@
 from flask import Flask, render_template, request
 
+import requests
+import os
+import uuid
+import json
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
-
-my_words = {"hello": "Привет", "cat": "Кот","hi":"Привет", "apple": "Яблоко",
-            "chair":"Стул", "table":"Стол", "leg":"Нога"}
-
-@app.route('/hello')
-def hello_world():
-    return "hello world"
-
-@app.route('/hello/<name>')
-def hello_with_name(name):
-    return f"hello {name}"
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def dict_page():
-    if request.method == 'POST':
-        word = request.form["word"].lower()
-        return render_template('result.html',word=my_words[word])
-
     return render_template('dict.html')
 
+
+@app.route('/translate', methods=['GET', 'POST'])
+def index_post():
+
+    langFrom = request.form['langFrom']
+    langTo = request.form['langTo']
+    original_text = request.form['word']
+    print(original_text)
+    target_language = 'ru'
+
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+    path = '/translate?api-version=3.0&'
+
+    target_language_parameter = 'from=' + langFrom + '&to=' + langTo
+
+    constructed_url = endpoint + path + target_language_parameter
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+
+    body = [{'text': original_text}]
+
+    translator_request = requests.post(
+        constructed_url, headers=headers, json=body)
+
+    translator_response = translator_request.json()
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    return render_template(
+        'result.html',
+        word=translated_text
+    )
 
 if __name__ == '__main__':
     app.run()
